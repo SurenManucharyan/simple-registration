f27a844325b549982c68cc787a56ed35a7dca794
SurenManucharyan <suren.manucharyan06@gmail.com>
2025-09-22 12:35:19 +0400

    rebase conflict

--- registration-form/src/components/rebase.jsx
+++
@@ -1,7 +0,0 @@
-console.log('====================================');
-console.log("sdcsdcsdcvsd");
-console.log('====================================');
-console.log("sdcsdfvsd");
-console.log('====================================');
-console.log("sdcvsdvsd");
-console.log('====================================');

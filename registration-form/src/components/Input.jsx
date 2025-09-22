fbc5b6c9cd20cea743cde9c5a4b48d9f85cfa698
SurenManucharyan <suren.manucharyan06@gmail.com>
2025-09-22 15:41:09 +0400

    Memoizing inputs

---
+++ registration-form/src/components/Input.jsx
@@ -0,0 +1,23 @@
+import React, { memo } from "react";
+
+const Input = memo(function Input({
+  type,
+  name,
+  placeholder,
+  value,
+  onChange,
+}) {
+    
+  return (
+    <input
+      type={type}
+      name={name}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      className="w-full px-3 py-2 mb-3 border rounded-md text-black"
+    />
+  );
+});
+
+export default Input;

bf5ce177e4c45cedb35d77f30ee2c4f458fb089a
SurenManucharyan <suren.manucharyan06@gmail.com>
2025-09-22 12:37:13 +0400

    sssss

---
+++ registration-form/src/components/rebase.jsx
@@ -0,0 +1,7 @@
+console.log('====================================');
+console.log("sdcsdcsdcvsd");
+console.log('====================================');
+console.log("sdcsdfvsd");
+console.log('====================================');
+console.log("sdcvsdvsd");
+console.log('====================================');

5f72aaf9ce78df18e33b0375ec32daad829064d5
SurenManucharyan <suren.manucharyan06@gmail.com>
2025-09-23 23:47:23 +0400

    zzz

--- registration-form/src/conflicts/MergeConflict.jsx
+++ registration-form/src/conflicts/MergeConflict.jsx
@@ -3,3 +3,6 @@ console.log("aaaaaa");
 console.log("aaaaaa");
 console.log("====================================");
 console.log("aaaa");
+console.log('====================================');
+console.log("zzzz");
+console.log('====================================');
